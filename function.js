4414234ab311a7ba3eb42fe3183a7dae83b32a63
swarup <swarup260@gmail.com>
2018-12-25 23:35:07 +0530

    reverse string and isCheckPalidrome functin added

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
 /* check the check occurrence  of element in sequences*/
-const checkOccurrence = (args,key) => {
+const checkOccurrence = (args, key) => {
     for (x of args) {
         if (x == key) {
             return true
@@ -9,13 +9,22 @@ const checkOccurrence = (args,key) => {
 }
 
 /* sum of a numbers */
-const sumOf = (number) => (number+1)*number/2 
+const sumOf = number => number * (number + 1) / 2
 
+/* check if the no. even or not */
+const isEven = number => (number & 1) == 0 ? true : false
 
-const isEven = (number) => {
-    if ((number & 1) == 0) {
-        return true
-    }else{
-        return false
+/* using for loop */
+const reverseStr = string => {
+    reverse = "";
+    for (let index = string.length - 1; index >= 0; index--) {
+        reverse += string[index]
     }
+    return reverse;
 }
+
+/* using inbuilt function */
+const reverseString = string => string.split("").reverse().join("")
+
+/* check if the string  */
+const isCheckPalidrome = string => string == reverseString(string) ? true : false
